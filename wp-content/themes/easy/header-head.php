066d45d150567a78f58819e74a54dd759b1e2a01
Cédric Denis <cedenis@gmail.com>
2023-04-02 16:52:12 +0200

    Sépare la balise head dans header-head.php

---
+++ wp-content/themes/easy/header-head.php
@@ -0,0 +1,36 @@
+<head>
+    <!-- title -->
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <!-- @todo Remplacer par wp_title() -->
+    <title>Vincent - Personal Portfolio Template</title>
+
+    <!-- description -->
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no, maximum-scale=1">
+    <meta name="author" content="Pixelshow">
+    <meta name="description" content="Personal Portfolio Template">
+
+    <!-- favicon -->
+    <link rel="shortcut icon" href="<?= get_stylesheet_directory_uri()?>/html/images/favicon.png" type="image/png">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="<?= get_stylesheet_directory_uri()?>/html/images/apple-touch-icon-114x114-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="<?= get_stylesheet_directory_uri()?>/html/images/apple-touch-icon-72x72-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="57x57" href="<?= get_stylesheet_directory_uri()?>/html/images/apple-touch-icon-57x57-precomposed.png">
+
+    <!-- fonts -->
+    <link href='http://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+    <link href="https://fonts.googleapis.com/css?family=Raleway:900" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Monoton" rel="stylesheet">
+
+    <!-- styles -->
+    <link rel="stylesheet" type="text/css" href="<?= get_stylesheet_directory_uri()?>/html/css/bootstrap.min.css"/>
+    <link rel="stylesheet" type="text/css" href="<?= get_stylesheet_directory_uri()?>/html/css/magnific-popup.min.css"/>
+    <link rel="stylesheet" type="text/css" href="<?= get_stylesheet_directory_uri()?>/html/css/font-awesome.min.css"/>
+    <link rel="stylesheet" type="text/css" href="<?= get_stylesheet_directory_uri()?>/html/css/style.css"/>
+
+    <!-- Javascript libraries -->
+    <script type="text/javascript" src="<?= get_stylesheet_directory_uri()?>/html/js/modernizr.js"></script>
+    <script type="text/javascript" src="<?= get_stylesheet_directory_uri()?>/html/js/jquery.min.js"></script>
+
+    <!--[if IE]>
+    <script src="<?= get_stylesheet_directory_uri()?>/html/js/html5shiv.js"></script>
+    <![endif]-->
+</head>
